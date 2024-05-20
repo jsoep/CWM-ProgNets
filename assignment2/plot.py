05f2b7a41ea9f5735507b1074ffe69ff82516824
jsoep <jsoepadmo@hotmail.sg>
2024-05-20 16:56:59 +0100

    Ex2 logs and plots

--- assignment2/plot.py
+++ assignment2/plot.py
@@ -3,23 +3,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # parameters to modify
-filename="test.data"
-label='label'
-xlabel = 'xlabel'
-ylabel = 'ylabel'
-title='Simple plot'
-fig_name='test.png'
-bins=100 #adjust the number of bins to your plot
+filename1="iperf3_q1proc.txt"
+#filename2="iperf_3sc.txt"
+label1=''
+#label2='server-client'
+xlabel = 'time (s)'
+ylabel = 'bandwidth (Mbps)'
+title='iperf3 Qu1'
+fig_name='iperf3_q1.png'
+#bins=10 #adjust the number of bins to your plot
 
 
-t = np.loadtxt(filename, delimiter=" ", dtype="float")
+t1 = np.loadtxt(filename1, delimiter=" ", dtype="float")
+#t2 = np.loadtxt(filename2, delimiter=" ", dtype="float")
 
-plt.plot(t[:,0], t[:,1], label=label)  # Plot some data on the (implicit) axes.
+plt.plot(t1[:,0], t1[:,1], label=label1)  # Plot some data on the (implicit) axes.
+#plt.plot(t2[:,0], t2[:,1], label=label2)
 #Comment the line above and uncomment the line below to plot a CDF
-#plt.hist(t[:,1], bins, density=True, histtype='step', cumulative=True, label=label)
+#plt.hist(t, bins, density=True, histtype='step', cumulative=True, label=label)
 plt.xlabel(xlabel)
 plt.ylabel(ylabel)
 plt.title(title)
-plt.legend()
+plt.legend(loc='best')
 plt.savefig(fig_name)
 plt.show()
